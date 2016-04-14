1ad7f3d4726d18b397f619c3c78361b730f1b738
pointdeveloper <pointdeveloper99@gmail.com>
2016-04-14 20:40:23 +0530

    port changed

--- serverExample.js
+++ serverExample.js
@@ -1,10 +1,10 @@
 var http=require("http");
-
+var port =process.env.PORT ||1337;
 var server = http.createServer(function(req,res) {
 res.writeHead(200,{"Content-type":"text/plain"});
 res.end("hellow world")
 });
 
-server.listen(3000);
+server.listen(port);
 
-console.log("server started on 3000");
+console.log("server started on "+port);
